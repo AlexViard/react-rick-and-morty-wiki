2fad73c73de4ebc6039f1a973c777a6d51049884
AlexViard <alex-viard@hotmail.fr>
2021-08-30 01:24:53 +0200

    clean app + create header

---
+++ src/components/App/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import Header from '../Header';
+import './style.scss';
+
+const App = () => (
+  <Header />
+);
+
+export default App;
+
---
+++ src/components/Header/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import './style.scss';
+
+const Header = () => (
+  <div className="header">
+    
+  </div>
+);
+
+export default Header;
