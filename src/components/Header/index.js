a5699e87303ee9405cc4199dfcb800dc27dbee7a
AlexViard <alex-viard@hotmail.fr>
2021-08-30 02:21:39 +0200

    create Navbar

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
-
+import NavBar from '../Navbar';
 import './style.scss';
 
 const Header = () => (
   <div className="header">
-    
+    <NavBar />
   </div>
 );
 
---
+++ src/components/Navbar/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+import logo from 'images/rick-and-morty-logo.png';
+import { Link } from 'react-router-dom';
+
+import './style.scss';
+
+const NavBar = () => (
+  <nav className="navbar">
+      <ul className="navbar-list">
+          <li className="navbar-list-link"><Link to="/">Personnages</Link></li>
+          <li className="navbar-list-link"><Link to="/">Forum</Link></li>
+          <li className="navbar-list-link img">
+            <img src={logo} alt="" />
+          </li>
+          <li className="navbar-list-link"><Link to="/">Episodes</Link></li>
+          <li className="navbar-list-link"><Link to="/">Connexion</Link></li>
+      </ul>
+  </nav>
+);
+
+export default NavBar;
