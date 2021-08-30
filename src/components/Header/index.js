17af458cfae3ed3184bc7a86e0b3c09ddaca1275
AlexViard <alex-viard@hotmail.fr>
2021-08-30 02:31:41 +0200

    add img header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -5,6 +5,9 @@ import './style.scss';
 const Header = () => (
   <div className="header">
     <NavBar />
+    <div className="header-img">
+      <img src="https://staticdelivery.nexusmods.com/mods/1151/images/528-0-1447526230.png" alt="" />
+    </div>
   </div>
 );
 
