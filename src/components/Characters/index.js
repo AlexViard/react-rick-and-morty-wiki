ca79ef4bb34a0e2554b740aa7f13433f0f0e9b53
AlexViard <alex-viard@hotmail.fr>
2021-08-31 23:45:15 +0200

    create characters page + add linear gradient and navbar

---
+++ src/components/Characters/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import NavBar from 'components/NavBar';
+// import SearchBar from 'components/SearchBar';
+import './style.scss';
+
+const Characters = () => (
+  <div className="characters">
+    <NavBar />
+    {/* <SearchBar /> */}
+  </div>
+);
+
+export default Characters;
